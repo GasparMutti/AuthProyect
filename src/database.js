3b3900478ae757fcfb0e29412efbe4af3d95c317
Notebook Gaspar <gasparrobortellamutti@gmail.com>
2023-01-18 16:37:44 -0300

    db update

--- src/database.js
+++ src/database.js
@@ -4,7 +4,9 @@ mongoose.set("strictQuery", false);
 
 (async () => {
   try {
-    const db = await mongoose.connect("mongodb://localhost/auth-proyect");
+    const db = await mongoose.connect(
+      "mongodb+srv://gmutti:08012003@cluster0.wkt25d7.mongodb.net/?retryWrites=true&w=majority"
+    );
     console.log("DB contected to", db.connection.name);
   } catch (error) {
     console.log(error);
